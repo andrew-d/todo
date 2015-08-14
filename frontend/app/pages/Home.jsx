5cbdded430c6253597ca629422ac4e57566cb4b8
Andrew Dunham <andrew@du.nham.ca>
2015-08-14 16:18:53 -0700

    Split Home rendering into different functions

--- frontend/app/pages/Home.jsx
+++ frontend/app/pages/Home.jsx
@@ -20,48 +20,83 @@ class Home extends React.Component {
     return (
       <div className='row'>
         <div className='col-md-3'>
-          <TodoListMenu>
-            <TodoListMenuItem
-              name='Main List'
-              totalItems={10}
-              completedItems={4}
-            />
-            <TodoListMenuItem
-              name='Other List 1'
-              totalItems={3}
-              completedItems={2}
-            />
-            <TodoListMenuItem
-              name='Other List 2'
-              totalItems={12}
-            />
-          </TodoListMenu>
+          {this.renderMenu()}
         </div>
 
         <div className='col-md-9'>
-          <TodoList listName='Main TODO List'>
-            <TodoListItem text='Item One' />
-            <TodoListItem text='Item Two' />
-            <TodoListItem text='Item Three' />
-            <TodoListItem text='Item Four' />
-            <TodoListItem text='Finished Item' complete={true} />
-          </TodoList>
-
-          <div className="progress">
-            <div
-              className="progress-bar progress-bar-success"
-              role="progressbar"
-              aria-valuenow="20"
-              aria-valuemin="0"
-              aria-valuemax="100"
-              style={{width: '20%'}}
-            >
-              <span>20% Complete</span>
-            </div>
-          </div>
+          {this.renderTodoListFor(0 /* TODO */)}
         </div>
       </div>
     );
+  }
+
+  renderMenu() {
+    const menuItems = [
+      {id: 1, name: 'Main List'},
+      {id: 2, name: 'Other List 1'},
+      {id: 3, name: 'Other List 2'},
+    ];
+
+    const renderedItems = menuItems.map((item) => (
+      <TodoListMenuItem
+        key={`menu-item-${item.id}`}
+        name={item.name}
+        totalItems={10}
+        completedItems={5}
+      />
+    ));
+
+    return (
+      <TodoListMenu>
+        {renderedItems}
+      </TodoListMenu>
+    );
+  }
+
+  renderTodoListFor(id) {
+    // TODO:
+    const listItems = [
+      {id: 1, text: 'Item One', complete: false},
+      {id: 2, text: 'Item Two', complete: false},
+      {id: 3, text: 'Item Three', complete: true},
+    ];
+
+    const renderedItems = listItems.map((item) => (
+      <TodoListItem
+        key={`todo-item-${item.id}`}
+        text={item.text}
+        complete={item.complete}
+      />
+    ));
+
+    // Calculate finished percentage.
+    const finishedItems = listItems.filter(i => i.complete).length,
+          finishedPercent = (finishedItems / listItems.length) * 100.0,
+          finishedString = finishedPercent.toFixed(2) + '%';
+
+    // TODO: make a component
+    const progressBar = (
+      <div key='progress' className="progress">
+        <div
+          className="progress-bar progress-bar-success"
+          role="progressbar"
+          aria-valuenow="20"
+          aria-valuemin="0"
+          aria-valuemax="100"
+          style={{width: finishedString}}
+        >
+          <span>{finishedString} Complete</span>
+        </div>
+      </div>
+    );
+
+
+    return [
+      <TodoList key='list' listName='Main TODO List'>
+        {renderedItems}
+      </TodoList>,
+      progressBar,
+    ];
   }
 }
 
